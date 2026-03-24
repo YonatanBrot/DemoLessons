d150ea090a24ddc064ded393ef05dcd05ddf18ef
Yonatan Vigiser <yonatanvigiser@gmail.com>
2026-03-24 13:52:24 +0200

    Change current limits & Calibrated interpolation

--- src/main/java/frc/robot/shooting/ShootingMeasurments.java
+++ src/main/java/frc/robot/shooting/ShootingMeasurments.java
@@ -4,12 +4,31 @@ import edu.wpi.first.math.geometry.Transform3d;
 
 public class ShootingMeasurments {
     public static final ShootingState[] ALL_MEASURMENTS_HUB = new ShootingState[] {
-        new ShootingState(1.5, 2000, 38),
-        new ShootingState(2, 2050, 40),
-        new ShootingState(2.5, 2150, 43),
-        new ShootingState(3, 2200, 45),
-        new ShootingState(3.5, 2300, 47.5),
-        new ShootingState(4, 2400, 49),
+        new ShootingState(1.2, 1850, 31),
+        new ShootingState(1.45, 1800, 34),
+        new ShootingState(1.7, 1900, 36),
+        new ShootingState(1.95, 2000, 38),
+        new ShootingState(2.2, 2030, 42),
+        new ShootingState(2.45, 2100, 42),
+        new ShootingState(2.7, 2160, 42),
+        new ShootingState(2.95, 2160, 43),
+        new ShootingState(3.2, 2180, 45),
+        new ShootingState(3.45, 2222, 45),
+        new ShootingState(3.7, 2300, 45),
+        new ShootingState(3.95, 2380, 47),
+        new ShootingState(4.2, 2430, 48),
+        new ShootingState(4.45, 2480, 49),
+        new ShootingState(4.7, 2530, 50),
+        new ShootingState(4.95, 2530, 51),
+        new ShootingState(5.2, 2650, 52),
+        new ShootingState(5.45, 2670, 51.5),
+        // new ShootingState(1.5, 1950, 39),
+        // new ShootingState(2, 2050, 42),
+        // new ShootingState(2.5, 2100, 44),
+        // new ShootingState(3, 2200, 45),
+        // new ShootingState(3.5, 2300, 47.5),
+        // new ShootingState(4, 2400, 49),
+        // new ShootingState(5.5, 2850, 52.5),
     };
     public static final ShootingState[] ALL_MEASURMENTS_DELIVRY = new ShootingState[]{
 
--- src/main/java/frc/robot/subsystems/roller/RollerConstants.java
+++ src/main/java/frc/robot/subsystems/roller/RollerConstants.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.roller;
 
 public class RollerConstants {
-    public static final int CURRENT_LIMIT = 40;
+    public static final int CURRENT_LIMIT = 30;
     public static final double MAX_VOLTAGE = 12;
 }

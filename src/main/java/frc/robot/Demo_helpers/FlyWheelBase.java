d4ef90ab66bf3c89f1fba9272087d6e8c92bcc40
YonatanBrot <yonatanbrot@gmail.com>
2026-05-31 15:45:22 +0300

    Added the logs (or, like, the lil thing we say is logs)

--- src/main/java/frc/robot/Demo_helpers/FlyWheelBase.java
+++ src/main/java/frc/robot/Demo_helpers/FlyWheelBase.java
@@ -1,6 +1,7 @@
 package frc.robot.Demo_helpers;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team2679.atlantiskit.logfields.LogFieldsTable;
 
 public abstract class FlyWheelBase extends SubsystemBase {
     public abstract void setSpeed(double speed);
@@ -14,4 +15,16 @@ public abstract class FlyWheelBase extends SubsystemBase {
     public abstract boolean isAtSpeed(double speed);
 
     public abstract void resetPID();
+
+    private LogFieldsTable logFieldsTable = new LogFieldsTable("Flywheel");
+
+    protected void log(String name, double value){
+        logFieldsTable.recordOutput(name, value);
+    }
+
+    public FlyWheelBase() {
+        logFieldsTable.addDouble("current 1", this::getMotor1Current);
+        logFieldsTable.addDouble("current 2", this::getMotor2Current);
+        logFieldsTable.addDouble("speed", this::getSpeed);
+    }
 }

8ff89e32c54c39828165982279e910876dd4923f
YonatanBrot <yonatanbrot@gmail.com>
2026-06-04 18:17:00 +0300

    Moved the unecassary constants

---
+++ src/main/java/frc/robot/Demo_helpers/Const.java
@@ -0,0 +1,14 @@
+package frc.robot.Demo_helpers;
+
+public class Const {
+    public static final double MAX_VOLTAGE = 12;
+    public static final double MIN_VOLTAGE = 12;
+
+    public static final double KP = 0.028107;
+    public static final double KD = 0.0;
+    public static final double KI = 0.0;
+
+    public static final double KS = 0.2654;
+    public static final double KV = 0.0018705;
+    public static final double KA = 0.0001328;    
+}

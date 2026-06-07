c4c66389377ee892915fb3879b8847ac4309297a
YonatanBrot <yonatanbrot@gmail.com>
2026-06-07 07:47:39 +0300

    Changed the ff and PID const class names to be more clear

--- src/main/java/frc/robot/subsystems/flywheel/Const.java
+++ src/main/java/frc/robot/subsystems/flywheel/Const.java
@@ -1,13 +1,13 @@
 package frc.robot.subsystems.flywheel;
 
 public class Const {
-    public static class kPID {
+    public static class PIDConstants {
         public static final double KP = 0.028107;
         public static final double KD = 0.0;
         public static final double KI = 0.0;
     }
 
-    public static class kFF {
+    public static class FeedForwardConstants {
         public static final double KS = 0.2654;
         public static final double KV = 0.0018705;
         public static final double KA = 0.0001328;    
